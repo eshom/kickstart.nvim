6ba143474895a2b756e01d1281b87bff5e895f5a
eshom <48328872+eshom@users.noreply.github.com>
2024-04-17 20:57:52 +0300

    fixed slime and some keybindings

--- init.lua
+++ init.lua
@@ -347,7 +347,7 @@ require('lazy').setup({
       end, { desc = '[S]earch [/] in Open Files' })
 
       -- Shortcut for searching your Neovim configuration files
-      vim.keymap.set('n', '<leader>sn', function()
+      vim.keymap.set('n', '<leader>fn', function()
         builtin.find_files { cwd = vim.fn.stdpath 'config' }
       end, { desc = '[S]earch [N]eovim files' })
     end,
@@ -554,7 +554,7 @@ require('lazy').setup({
     lazy = false,
     keys = {
       {
-        '<leader>f',
+        '<leader>bf',
         function()
           require('conform').format { async = true, lsp_fallback = true }
         end,
