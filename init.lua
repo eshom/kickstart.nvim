86f30c38186b8d8482e0291b31deb73c98d16971
eshom <48328872+eshom@users.noreply.github.com>
2024-05-17 12:14:41 +0300

    fix tabs

--- init.lua
+++ init.lua
@@ -5,8 +5,8 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- tabs indentation and spaces
+vim.o.tabstop = 8
 vim.o.shiftwidth = 4
-vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.expandtab = true
 vim.o.smarttab = true
