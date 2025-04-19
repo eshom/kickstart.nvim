f44c2b829f8f8841b45944ec8f6e5ac42c8e4d13
eshom <48328872+eshom@users.noreply.github.com>
2025-04-19 13:55:39 +0300

    don't need the ripgrep command when there is <leader>sg

--- lua/custom/commands.lua
+++ lua/custom/commands.lua
@@ -3,11 +3,11 @@ local builtin = require 'telescope.builtin'
 return {
   -- Grep to quickfix
 
-  vim.api.nvim_create_user_command('Grep', function(args)
-    local rg_out = vim.fn.systemlist('rg ' .. '--vimgrep ' .. args.args)
-    vim.fn.setqflist({}, ' ', { lines = rg_out, title = 'Grep results' })
-    builtin.quickfix()
-  end, { nargs = '*', desc = 'RipGrep working directory and open result in quickfix list' }),
+  -- vim.api.nvim_create_user_command('Grep', function(args)
+  --   local rg_out = vim.fn.systemlist('rg ' .. '--vimgrep ' .. args.args)
+  --   vim.fn.setqflist({}, ' ', { lines = rg_out, title = 'Grep results' })
+  --   builtin.quickfix()
+  -- end, { nargs = '*', desc = 'RipGrep working directory and open result in quickfix list' }),
 
   vim.api.nvim_create_user_command('ZlsDevStart', function()
     -- Stop auto-started zls clients if using zls-dev
