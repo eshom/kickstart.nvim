85627e7c91af22b0a05632417f64e98469e2092c
eshom <48328872+eshom@users.noreply.github.com>
2025-05-17 18:27:43 +0300

    refactor: pretty much done

--- init.lua
+++ init.lua
@@ -1,18 +1,12 @@
+-- Not lazy modules
 require 'options'
 require 'keymap'
 require 'autocmd'
 require 'lazy-install'
 
+-- Lazy section
 require('lazy').setup({
-  require 'kickstart.plugins.debug',
-  require 'kickstart.plugins.indent_line',
-  require 'kickstart.plugins.gitsigns',
-
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    This is the easiest way to modularize your config.
-  --
-  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
+  -- Additional lazy imports
   { import = 'custom.plugins' },
   { import = 'custom' },
 }, {
@@ -36,6 +30,3 @@ require('lazy').setup({
     },
   },
 })
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
