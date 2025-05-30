426656ae46be4fd00ede9490976df1005e56ecb3
eshom <48328872+eshom@users.noreply.github.com>
2025-05-30 15:19:38 +0300

    automatically use zls-dev if correct version detected

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -18,3 +18,58 @@ vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead', 'FileType' }, {
     vim.o.filetype = 'zig'
   end,
 })
+
+--- Returns existing client id and root dir
+---@return { id: integer, root_dir: string }?
+local find_zls_id = function()
+  local clients = vim.lsp.get_clients()
+  for _, value in pairs(clients) do
+    if value.name == 'zls' then
+      local out = {}
+      out.id = value.id
+      if value.root_dir == nil then
+        return nil
+      end
+      out.root_dir = value.root_dir
+      return out
+    end
+  end
+end
+
+--- Lookup `minimum_zig_version` in `build.zig.zon`. If it doesn't exist
+--- or if there is no "dev" contained in the string returns `false`.
+--- Otherwise returns `true`
+---@param root_dir string
+---@return boolean
+local is_zig_dev = function(root_dir)
+  local build_zig_zon = root_dir .. '/build.zig.zon'
+  local zon = io.open(build_zig_zon, 'r')
+  if zon == nil then
+    return false
+  end
+  --`build.zig.zon` files are not expected to be big, so reading all to memory is fine.
+  ---@type string
+  local contents = zon:read '*a'
+  local find_dev = contents:find '%.minimum_zig_version.+%d+%.%d+%.%d+-dev'
+  zon:close()
+  return find_dev ~= nil
+end
+
+-- Use `zls-dev` instead of `zls` if `.minimum_zig_version` contains "dev"
+vim.api.nvim_create_autocmd({ 'LspAttach' }, {
+  desc = 'Use zls-dev automatically if relevant',
+  pattern = { '*.zig', '*.zon' },
+  group = vim.api.nvim_create_augroup('zig', { clear = false }),
+  callback = function()
+    local zls_existing = find_zls_id()
+    if zls_existing == nil then
+      return
+    end
+    if is_zig_dev(zls_existing.root_dir) then
+      -- `ZlsDevStart` also stops zls if it finds it.
+      -- Keeping it in case I want to manually run the command
+      vim.lsp.stop_client(zls_existing.id)
+      vim.api.nvim_cmd({ cmd = 'ZlsDevStart' }, {})
+    end
+  end,
+})
--- lua/custom/commands.lua
+++ lua/custom/commands.lua
@@ -2,17 +2,20 @@
 
 return {
   vim.api.nvim_create_user_command('ZlsDevStart', function()
-    -- Stop auto-started zls clients if using zls-dev
+    -- Stop previously auto-started zls clients if using zls-dev
+    ---@type string
+    local root_dir
     local clients = vim.lsp.get_clients()
     for _, value in pairs(clients) do
       if value.name == 'zls' then
         vim.lsp.stop_client(value.id)
+        root_dir = value.root_dir
       end
     end
     vim.lsp.start {
       name = 'zls-dev',
       cmd = { 'zls-dev' },
-      root_dir = vim.fs.dirname(vim.fs.find({ 'build.zig', 'build.zig.zon' }, { upwards = true, stop = vim.loop.os_homedir() })[1]),
+      root_dir = root_dir,
       settings = {
         zls = {
           zig_exe_path = '/home/erez/.local/bin/zig-dev',
