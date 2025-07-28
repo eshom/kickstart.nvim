a6eb2f2d153e499985f4415f7dbe0cb50fd79a85
eshom <48328872+eshom@users.noreply.github.com>
2025-07-28 09:02:20 +0300

    fix zig path again

--- lua/custom/commands.lua
+++ lua/custom/commands.lua
@@ -20,8 +20,8 @@ return {
         zls = {
           -- zig_exe_path = '/home/erez/.local/bin/zig-dev',
           -- zig_lib_path = '/home/erez/.local/share/zigup/master/files/lib',
-          zig_exe_path = '/home/erez/src/zig/build/stage3/bin/zig',
-          zig_lib_path = '/home/erez/src/zig/build/stage3/lib/zig',
+          zig_exe_path = '/home/erez/.local/share/zig/zig-dev/bin/zig',
+          zig_lib_path = '/home/erez/.local/share/zig/zig-dev/lib/zig',
           enable_argument_placeholders = false,
         },
       },
